366fff78daebffee2721751ded434c182fea7040
Jose Ernesto is back <martinsjoseernesto@gmail.com>
2023-07-17 16:08:17 -0300

    Added 2 batch files (original and compiled) and the cleanup cmdlet

---
+++ recconsole.bat
@@ -0,0 +1,50 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJEuB90E/JghbQAHMOXO/Zg==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+echo FrontRub Recovery Console for Windows
+ver
+choice /C:YN /M:"Do you want to compress system files to save space?"
+IF ERRORLEVEL ==1 GOTO Compress
+IF ERRORLEVEL ==2 GOTO Finish
+GOTO next
+
+:Compress
+Compact.exe /CompactOS:query
+Compact.exe /CompactOS:always
+
+:Finish
+echo The compress operation completed successfully.
+
+:next
+cmd.exe /k cd C:\
