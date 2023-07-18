35845ecd3ef43405bb9d273d173692ce9567ecdf
Jose Ernesto is back <martinsjoseernesto@gmail.com>
2023-07-17 22:56:10 -0300

    Update recconsole.bat

--- recconsole.bat
+++ recconsole.bat
@@ -1,36 +1,4 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCuDJEuB90E/JghbQAHMOXO/Zg==
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
+
 @echo off
 echo FrontRub Recovery Console for Windows
 ver
@@ -40,8 +8,7 @@ IF ERRORLEVEL ==2 GOTO Finish
 GOTO next
 
 :Compress
-Compact.exe /CompactOS:query
-Compact.exe /CompactOS:always
+compact /c /s
 
 :Finish
 echo The compress operation completed successfully.
